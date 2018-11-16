4c23ed8db18c4a5de262aee136ddfe56456390e9
shiweibw <sugyswang@126.com>
2018-11-16 23:23:57 +0800

    version2

--- SortPhoto.py
+++ SortPhoto.py
@@ -1,27 +1,64 @@
 
 #--*-- coding:  utf-8 --*--
 
-import os, os.path
+import os, os.path, shutil
 
-PICUTRE = ['jpg']
-VIDEO = ['mpt']
+PICUTRE = ['.jpg']
+VIDEO = ['.mp4']
 
 class SortData():
-    def __init__(self,datapath):
-        self.datapath = datapath
+    def __init__(self,dst_dir):
+        self.dst_dir = dst_dir
     
     #在datapath目录下生成新的目录
-    def generateDir(self):
-        pass
+    def movePicture(self,year,month,type,file):
+        #在datapath目录下创建年度目录
+        year_dir = os.path.join(self.dst_dir,year)
+        month_dir = os.path.join(year_dir,month)
+        video_dir = os.path.join(month_dir,'VIDEO')
+        picture_dir = os.path.join(month_dir,'PICTURE')
+        
+        '''for dir in [year_dir, month_dir, video_dir, picture_dir]:
+            if not os.path.isdir(dir):
+                os.mkdir(dir)
+        '''        
+        if type in PICUTRE:
+            # move pciture to right dir
+            if not os.path.isdir(picture_dir):
+                os.makedirs(picture_dir)
+            shutil.move(file, picture_dir)
+        elif type in VIDEO:
+            if not os.path.isdir(video_dir):
+                os.makedirs(video_dir)
+            shutil.move(file, video_dir)
+        else:
+            print("Doesn't move %s" % file)
+            
+            
+
+        
+        
+        
+    #分析照片，得出照片的年份 月份和类型。
+    def AnalyPhoto(self,picture):
+        file = os.path.basename(picture)
+        filename,type = os.path.splitext(file)
+        pic = filename.split('_')[1]
+        assert len(pic[:4]) == 4, "the length of the YEAR must be 4 "
+        assert int(pic[4:6]) < 13, "MONTH must be 0 ~ 12 "
+        
+        return pic[:4],pic[4:6],type
     
     # 遍历datapath
-    def walkdir(self):
-        for file in os.listdir(path):
-            filepath = os.path.join(path,file)
-            if not os.path.isdir(filepath):
-                print(filepath)
+    def walkdir(self,datapath):
+        for file in os.listdir(datapath):
+            filepath = os.path.join(datapath,file)
+            if os.path.isfile(filepath):
+                year,month,type = self.AnalyPhoto(filepath)
+                self.movePicture(year,month,type,filepath)
+                
             else:
-                walkdir(filepath)
+                self.walkdir(filepath)
                 
     
             
@@ -29,7 +66,12 @@ class SortData():
 
             
 if __name__ == '__main__':
-    walkdir('D:\\Nobia')
+    a = SortData('D:\\Nobia')
+    a.walkdir('D:\\Nobia')
+    #a.generateDir('2018','02')
+    #y,m,t = a.AnalyPhoto('IMG_20170613_134101.jpg')
+    #print(y,m,t)
+    
         
             
     
